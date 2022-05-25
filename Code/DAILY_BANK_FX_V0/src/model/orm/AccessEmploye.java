590a0bef22807ec273764f525a5a08611d1f4578
Tilian HURÉ <58342016+Tilian-HURE@users.noreply.github.com>
2022-05-25 18:30:25 +0200

    Add files via upload

--- Code/DAILY_BANK_FX_V0/src/model/orm/AccessEmploye.java
+++ Code/DAILY_BANK_FX_V0/src/model/orm/AccessEmploye.java
@@ -1,12 +1,13 @@
 package model.orm;
 
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
-
-import model.data.Client;
+import application.tools.ConstantesIHM;
+import javafx.scene.control.Alert;
 import model.data.Employe;
 import model.orm.exception.DataAccessException;
 import model.orm.exception.DatabaseConnexionException;
@@ -14,36 +15,32 @@ import model.orm.exception.Order;
 import model.orm.exception.RowNotFoundOrTooManyRowsException;
 import model.orm.exception.Table;
 
+
 public class AccessEmploye {
 
-	public AccessEmploye() {
-	}
+	public AccessEmploye() {}
 
+	
 	/**
-	 * Recherche d'un employe par son login / mot de passe.
-	 *
-	 * @param login    login de l'employé recherché
-	 * @param password mot de passe donné
-	 * @return un Employe ou null si non trouvé
+	 * Recherche un employé par son login et mot-de-passe.
+	 * @param login : login de connexion
+	 * @param password : mot-de-passe de connexion
+	 * @return un Employe, ou null si non-trouvé
 	 * @throws RowNotFoundOrTooManyRowsException
 	 * @throws DataAccessException
 	 * @throws DatabaseConnexionException
 	 */
-	public Employe getEmploye(String login, String password)
-			throws RowNotFoundOrTooManyRowsException, DataAccessException, DatabaseConnexionException {
+	public Employe getEmploye(String login, String password) throws RowNotFoundOrTooManyRowsException, DataAccessException, DatabaseConnexionException {
 		Employe employeTrouve;
-
 		try {
 			Connection con = LogToDatabase.getConnexion();
 			String query = "SELECT * FROM EMPLOYE WHERE" + " login = ?" + " AND motPasse = ?";
-
 			PreparedStatement pst = con.prepareStatement(query);
 			pst.setString(1, login);
 			pst.setString(2, password);
-
 			ResultSet rs = pst.executeQuery();
 
-			System.err.println(query);
+			//System.err.println(query);
 
 			if (rs.next()) {
 				int idEmployeTrouve = rs.getInt("idEmploye");
@@ -54,9 +51,17 @@ public class AccessEmploye {
 				String motPasseTROUVE = rs.getString("motPasse");
 				int idAgEmploye = rs.getInt("idAg");
 				String estInactif = rs.getString("estInactif");
-
+				
 				employeTrouve = new Employe(idEmployeTrouve, nom, prenom, droitsAccess, loginTROUVE, motPasseTROUVE,
 						idAgEmploye, estInactif);
+				
+				if (ConstantesIHM.estInactif(employeTrouve)) {
+					Alert alert = new Alert(Alert.AlertType.ERROR);
+					alert.setTitle("Confirmation");
+					alert.setContentText("Ce compte a été désactivé.");
+					alert.showAndWait();
+					return null;
+				}
 			} else {
 				rs.close();
 				pst.close();
@@ -79,10 +84,10 @@ public class AccessEmploye {
 		}
 	}
 	
+
 	/**
-	 * Insertion d'un employé.
-	 *
-	 * @param client IN/OUT Tous les attributs IN sauf idEmploye en OUT
+	 * Insert un employé.
+	 * @param client IN/OUT : employé à insérer (tous les attributs IN sauf idEmploye en OUT)
 	 * @throws RowNotFoundOrTooManyRowsException
 	 * @throws DataAccessException
 	 * @throws DatabaseConnexionException
@@ -90,7 +95,6 @@ public class AccessEmploye {
 	public void insertEmploye(Employe employe)
 			throws RowNotFoundOrTooManyRowsException, DataAccessException, DatabaseConnexionException {
 		try {
-
 			Connection con = LogToDatabase.getConnexion();
 
 			String query = "INSERT INTO EMPLOYE VALUES (" + "seq_id_employe.NEXTVAL" + ", " + "?" + ", " + "?" + ", "
@@ -104,7 +108,7 @@ public class AccessEmploye {
 			pst.setInt(6, employe.idAg);
 			pst.setString(7, employe.estInactif);
 
-			System.err.println(query);
+			//System.err.println(query);
 
 			int result = pst.executeUpdate();
 			pst.close();
@@ -117,27 +121,26 @@ public class AccessEmploye {
 
 			query = "SELECT seq_id_employe.CURRVAL from DUAL";
 
-			System.err.println(query);
+			//System.err.println(query);
+			
 			PreparedStatement pst2 = con.prepareStatement(query);
 
 			ResultSet rs = pst2.executeQuery();
 			rs.next();
 			int numEmpBase = rs.getInt(1);
-
 			con.commit();
 			rs.close();
 			pst2.close();
-
 			employe.idEmploye = numEmpBase;
 		} catch (SQLException e) {
 			throw new DataAccessException(Table.Employe, Order.INSERT, "Erreur accès", e);
 		}
 	}
 	
+
 	/**
-	 * Mise à jour d'un employé.
-	 *
-	 * @param employe IN employe.idEmploye (clé primaire) doit exister
+	 * Met à jour un employé.
+	 * @param employe IN : employé à modifier (employe.idEmploye (clé primaire) doit exister)
 	 * @throws RowNotFoundOrTooManyRowsException
 	 * @throws DataAccessException
 	 * @throws DatabaseConnexionException
@@ -149,7 +152,6 @@ public class AccessEmploye {
 
 			String query = "UPDATE EMPLOYE SET " + "nom = " + "? , " + "prenom = " + "? , " + "droitsAccess = "
 					+ "? , " + "login = " + "? , " + "motPasse = " + "? , " + "estInactif = " + "?" + " " + "WHERE idEmploye = ? ";
-
 			PreparedStatement pst = con.prepareStatement(query);
 			pst.setString(1, employe.nom);
 			pst.setString(2, employe.prenom);
@@ -159,7 +161,7 @@ public class AccessEmploye {
 			pst.setString(6, employe.estInactif);
 			pst.setInt(7, employe.idEmploye);			
 
-			System.err.println(query);
+			//System.err.println(query);
 
 			int result = pst.executeUpdate();
 			pst.close();
@@ -174,14 +176,14 @@ public class AccessEmploye {
 		}
 	}
 	
+	
 	/**
-	 * Recherche des employés paramétrés (tous/un seul par id/par nom-prénom).
-	 *
-	 * @param idAg        : id de l'agence dont on cherche les employés
-	 * @param idNumCli    : vaut -1 si il n'est pas spécifié sinon numéro recherché
-	 * @param debutNom    : vaut "" si il n'est pas spécifié sinon sera le nom/prenom recherchés
+	 * Recherche des employés paramétrés (tous/un seul par id/par nom-prénom). /!\LA RECHERCHE PAR PRENOM NE MARCHE PAS/!\
+	 * @param idAg : id de l'agence dont on cherche les employés
+	 * @param idNumCli : vaut -1 si il n'est pas spécifié sinon numéro recherché
+	 * @param debutNom : vaut "" si il n'est pas spécifié sinon sera le nom/prenom recherchés
 	 * @param debutPrenom cf. @param debutNom
-	 * @return Le ou les employés recherchés, liste vide si non trouvé
+	 * @return ArrayList du ou des employés recherchés, vide si non-trouvé(s)
 	 * @throws DataAccessException
 	 * @throws DatabaseConnexionException
 	 */
@@ -191,10 +193,9 @@ public class AccessEmploye {
 
 		try {
 			Connection con = LogToDatabase.getConnexion();
-
 			PreparedStatement pst;
-
 			String query;
+			
 			if (idEmploye != -1) {
 				query = "SELECT * FROM EMPLOYE where idAg = ?";
 				query += " AND idEmploye = ?";
@@ -213,13 +214,15 @@ public class AccessEmploye {
 				pst.setInt(1, idAg);
 				pst.setString(2, debutNom);
 				pst.setString(3, debutPrenom);
+				
 			} else {
 				query = "SELECT * FROM Employe where idAg = ?";
 				query += " ORDER BY nom";
 				pst = con.prepareStatement(query);
 				pst.setInt(1, idAg);
 			}
-			System.err.println(query + " nom : " + debutNom + " prenom : " + debutPrenom + "#");
+			
+			//System.err.println(query + " nom : " + debutNom + " prenom : " + debutPrenom + "#");
 
 			ResultSet rs = pst.executeQuery();
 			while (rs.next()) {
@@ -244,7 +247,7 @@ public class AccessEmploye {
 		} catch (SQLException e) {
 			throw new DataAccessException(Table.Employe, Order.SELECT, "Erreur accès", e);
 		}
-
 		return alResult;
 	}
+	
 }
